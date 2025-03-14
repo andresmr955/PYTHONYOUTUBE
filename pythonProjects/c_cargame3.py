d08e929e03102fa9d97ef6afb73afecb7ba77898
Andres Marquez <andresmr955@gmail.com>
2025-03-14 12:28:40 -0400

    Turn on and turn off the car

--- pythonProjects/c_cargame3.py
+++ pythonProjects/c_cargame3.py
@@ -1,21 +1,29 @@
 ###DRY: DON'T REPEAT YOURSELF
-
-user_option = input("").lower()
-while user_option != "quit":
+car_on = False
+user_option = ""
+while True:
+    user_option = input("").lower()
     if user_option == "start":
-        print("Car started. Ready to go!")
-        user_option = input("").lower()
+        if user_option == "start" and car_on == True:
+            print("Car is already started")
+        elif user_option == "start":
+            car_on = True
+            print("Car started. Ready to go!")
     elif user_option == "stop":
-        print("Car stopped")
-        user_option = input("").lower()
+        if user_option == "stop" and car_on == False:
+            print("Car is already stopped")
+        elif user_option == "stop":
+            car_on = False
+            print("Car stopped!")
     elif user_option == "help":
         print('''
 start - to start the car
 stop - to stop the car 
 quit - to quit
 ''')
-        user_option = input("").lower()
+    elif user_option == "quit":
+        print("Finished process code 0")   
+        break       
     else:
-        print("Sorry, I don't understand")    
-        break
-print("Finished procces code 0")        
+        print("Sorry, I don't understand")  
+ 
