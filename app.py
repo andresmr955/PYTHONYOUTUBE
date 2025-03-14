29171f3236b6aa8cb40c84ecae34e52d73714f35
Andres Marquez <andresmr955@gmail.com>
2025-03-14 08:56:37 -0400

    learning and adding while loop

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ import math
 print(math.ceil(2.9))
 #minute 59:44
 
-##CONDTIONAL STATEMENTS
+##CONDITIONAL STATEMENTS
 
 is_hot = False
 is_cold = True
@@ -239,3 +239,50 @@ if has_good_credit and not has_criminal_record:
     print("Elegible for loan, cause it isn't a criminal")
 else:
     print("Not elegible for loan, because it is a criminal")
+
+    ## Comparison Operators
+## ==, !=, >, <, >=, <=
+
+temperature = 30
+
+if temperature > 30: 
+    print("It's a really hot day")
+elif temperature == 30:
+    print("It's a hot day")
+elif temperature < 30:
+    print("It is a cold day")
+
+
+## Exercise
+
+name = input("I am going to tell you, how many characters has your name, so please tell me What's your name?")
+
+if len(name) < 3: 
+    print("Your name it should be at least 3 characters")
+elif len(name) > 50: 
+    print("Your name can be a maximum of 50 characters")
+else:
+    print(f'Your name is good, {name}, and the length of your name is {len(name)}')
+
+    #WHILELOOPS
+
+#While condition:
+#    do something ...
+
+#Example
+
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+print("Well Done")
+
+
+#Example repeat a string
+j = 1
+i = 1
+while i <= 5:
+    print('*' * i)
+    i += 1
+    
+print("Well Done")
